c46df6b173c126410af9f7650c9b833532575391
Isabela Rodrigues <68721253+isavrodrigues@users.noreply.github.com>
2025-04-23 19:34:27 +0000

    alt

--- atv/09-exec/immortal.c
+++ atv/09-exec/immortal.c
@@ -1,1 +1,25 @@
-// implemente aqui!
+/* POSIX headers */
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+/* Standard C */
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+    if(argc != 2){
+        printf("Use o programa assim: $ ./immortal ./path/para/algum/programa\n");
+    }
+    else{
+
+
+    //eu passo o programa que eu quero chamar
+        char *args[] = {argv[1], NULL};
+        
+        if (execvp(args[0], args) == -1) {
+            perror("execvp failed!");
+            exit(EXIT_FAILURE);
+        }
+    }
+}
--- atv/10-entrada-saida/tirar_comentarios.c
+++ atv/10-entrada-saida/tirar_comentarios.c
@@ -1,8 +1,72 @@
+/* POSIX headers */
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+/* Standard C */
 #include <stdio.h>
 
-// crie suas funções aqui
+
 
 int main(int argc, char *argv[])
-{
+{   
+    if(argc != 3){
+        printf("Use o programa assim: ./tirar_comentarios caso1.c resp_caso1.c\n");
+    }
+    else{
+    //O_RDONLY = so le
+    int fd1 = open(argv[1], O_RDONLY);
+    //O_WRONLY = abrir/escrita
+    //O_CREAT = criar se nao existir
+    //0700 = da permissao escrita leitura e execucao
+    int fd2 = open(argv[2], O_WRONLY | O_CREAT, 0700);
+
+    int ret;
+    char x[2];
+
     // implemente aqui
+    estado1: ret = read(fd1, x, 1);
+            if(ret == 0) goto estado_fim;
+            if(x[0] == '/') goto estado2;
+            else write(fd2, x, 1);
+            goto estado1;
+
+
+    estado2: ret = read(fd1, x, 1);
+            if(ret == 0) goto estado_fim;
+            if(x[0] == '/') goto estado3;
+            else if(x[0] == '*') goto estado4;
+            else{
+                write(fd2, "/", 1);
+                write(fd2, x, 1);
+                goto estado1;
+            }
+
+    estado3:ret = read(fd1, x, 1);
+            if(ret == 0) goto estado_fim;
+            if(x[0] == '\n'){
+            write(fd2, "\n", 1);
+            goto estado1;}
+            else goto estado3;
+
+    estado4: ret = read(fd1, x, 1);
+            if(ret == 0) goto estado_fim;
+            if(x[0] == '*') goto estado5;
+            else goto estado4;
+
+
+    estado5:ret = read(fd1, x, 1);
+            if(ret == 0) goto estado_fim;
+            if(x[0] == '/') goto estado1;
+            else goto estado4;
+
+    estado_fim:
+
+    
+    close(fd1);
+    close(fd2);
+    }
+    
+    return 0;
 }
